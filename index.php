fb1d3327d15602a4aab7a1301411163501f859d2
Janzen Faidiban <janzenfaidiban@gmail.com>
2023-04-11 19:18:35 +0900

    update tailwind index.html

---
+++ index.php
@@ -0,0 +1,45 @@
+<!doctype html>
+<html>
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="https://cdn.tailwindcss.com"></script>
+</head>
+
+<body>
+    
+    <!-- HEADER START -->
+    <header class="border border-gray-600 container mx-auto p-6">
+        <h1>SaCode News</h1>
+        <p>Berbagai berita terbaru seputar SaCode</p>
+    </header>
+    <!-- HEADER END -->
+
+    <!-- NAV START -->
+    <nav class="border border-gray-600 container mx-auto p-6">
+        <a href="">Beranda</a> 
+        <a href="">Teknologi</a>
+        <a href="">Pendidikan</a>
+        <a href="">Pengembangan Diri</a>
+        <a href="">Kursus</a>
+        <a href="">Komunitas</a>
+        <a href="">Lainnya</a>
+    </nav>
+    <!-- NAV END -->
+
+    <!-- MAIN CONTENT START -->
+    <section class="border border-gray-600 container mx-auto p-6">
+    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sequi quam minima sunt aperiam quisquam? Necessitatibus, voluptatibus sequi! Neque, velit quos vel minus laborum fugiat ex voluptatibus itaque? Atque eveniet saepe unde mollitia ut facere error ab beatae sint numquam sapiente quaerat pariatur qui deserunt molestiae sit expedita quo veritatis, soluta incidunt perferendis laborum vitae suscipit nesciunt. Magnam atque vero voluptatem deleniti. Ea esse quas nam, magni animi, praesentium, atque voluptatem alias illo recusandae minima odio nobis deleniti culpa ipsa laborum dolores aspernatur? Exercitationem eum cumque autem quibusdam facere tempora aliquam nostrum libero doloribus odit inventore culpa fugit veniam reprehenderit aspernatur, voluptatum corporis saepe adipisci laudantium. Corrupti id quidem fugiat. Eveniet omnis veniam autem iusto. Veritatis rerum earum id nam vitae ducimus adipisci. Expedita, omnis, alias iure voluptatem in numquam accusamus provident dignissimos fuga debitis dolorum laboriosam dolor doloribus, quae inventore cum soluta. Eaque molestiae iure tempore at accusantium ratione nobis tenetur voluptatibus doloremque. Error et corrupti repellat dignissimos provident, iusto facilis ex ipsam dolores officiis cum quo blanditiis tenetur nam neque autem consequatur debitis id a totam. Tenetur explicabo minus voluptas laudantium eos odio dolor blanditiis, fuga eligendi voluptates beatae aliquam nesciunt quia veniam molestiae illo. Tempore illo officiis libero.</p>
+    </section>
+    <!-- MAIN CONTENT END -->
+
+    <!-- FOOTER START -->
+    <footer class="border border-gray-600 container mx-auto p-6">
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Error perspiciatis consequatur odio, impedit quibusdam sit labore, omnis aut nihil perferendis harum eius? Perspiciatis iste nobis dignissimos fuga quasi maxime accusamus.</p>
+    </footer>
+    <!-- FOOTER END -->
+
+</body>
+
+</html>
